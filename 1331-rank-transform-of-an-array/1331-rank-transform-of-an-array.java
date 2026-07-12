fd08350845cdaac67570da9ef906bc68ad06d450
raguram <151853868+raguram5105@users.noreply.github.com>
2026-07-12 10:53:27 +0530

    Time: 29 ms (84.49%), Space: 76.9 MB (38.3%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] t=arr.clone();
+        Arrays.sort(t);
+        HashMap<Integer, Integer> rank=new HashMap<>();
+        int r=1;
+        for(int num:t){
+            if(!rank.containsKey(num)){
+                rank.put(num,r++);
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=rank.get(arr[i]);
+        }return arr;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
