c4d714368d541e4ef03f28e439add01017a30345
raguram <151853868+raguram5105@users.noreply.github.com>
2026-08-08 21:05:12 +0530

    Time: 9 ms (10.16%), Space: 47.7 MB (11.39%) - LeetHub - Time: O(n*m), Space: O(n*m)

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int islandPerimeter(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
+    public int islandPerimeter(int[][] g) {
+        int n=g.length;
+        int m=g[0].length;
         Queue<int []> q=new LinkedList<>();
-        int[][] di={{-1,0},{1,0},{0,-1},{0,1}};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
-                    q.offer(new int[] {i,j});
+                if(g[i][j]==1){
+                    q.offer(new int[]{i,j});
                 }
             }
         }
+        int[][] dir={{-1,0},{1,0},{0,-1},{0,1}};
         int t=0;
         while(!q.isEmpty()){
             int[] no=q.poll();
             int x=no[0];
             int y=no[1];
-
             int c=4;
-            for(int[] d:di){
+            for(int[] d:dir){
                 int dx=x+d[0];
                 int dy=y+d[1];
-                if(dx>=0 && dx<n && dy>=0 && dy<m && grid[dx][dy]==1 ){
+                if(dx>=0 && dx<n && dy>=0 && dy<m && g[dx][dy]==1){
                     c--;
                 }
-            }t+=c;
+            }
+            t+=c;
         }return t;
     }
 }
