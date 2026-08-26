f9c1a1c6604086076706f907681397ca6237150e
raguram <151853868+raguram5105@users.noreply.github.com>
2026-08-26 16:13:35 +0530

    Time: 8 ms (10.44%), Space: 51.2 MB (92.29%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numIslands(char[][] g) {
+        int n=g.length;
+        int m=g[0].length;
+        int c=0;
+        int[][] dir={{1,0},{-1,0},{0,1},{0,-1}};
+        Queue<int[]> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(g[i][j]=='1'){
+                    g[i][j]='0';
+                    q.offer(new int[]{i,j});
+                    while(!q.isEmpty()){
+                        int[] no=q.poll();
+                        int x=no[0];
+                        int y=no[1];
+                        for(int[] d:dir){
+                            int dx=x+d[0];
+                            int dy=y+d[1];
+                            if(dx>=0 && dx<n && dy>=0 && dy<m && g[dx][dy]=='1'){
+                                q.offer(new int[]{dx,dy});
+                                g[dx][dy]='0';
+                            }
+                        }
+                    }c++;
+                }
+            }
+        }
+        return c;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
